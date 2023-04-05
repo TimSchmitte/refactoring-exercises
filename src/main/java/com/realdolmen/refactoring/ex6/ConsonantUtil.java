2132f1af905a66e41f6bce6bd8d49e7d25925a9b
tims <tim.schmitte@realdolmen.com>
2023-04-05 11:00:21 +0200

    ex6: solution part 2

--- src/main/java/com/realdolmen/refactoring/ex6/ConsonantUtil.java
+++ src/main/java/com/realdolmen/refactoring/ex6/ConsonantUtil.java
@@ -2,6 +2,6 @@ package com.realdolmen.refactoring.ex6;
 
 public class ConsonantUtil {
     static boolean isAConsonant(char c) {
-        return c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && c != 'y' && c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U' && c != 'Y';
+        return String.valueOf(c).matches("[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]");
     }
 }
--- src/test/java/com/realdolmen/refactoring/ex6/ConsonantUtilTest.java
+++ src/test/java/com/realdolmen/refactoring/ex6/ConsonantUtilTest.java
@@ -1,8 +1,8 @@
 package com.realdolmen.refactoring.ex6;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConsonantUtilTest {
@@ -17,5 +17,17 @@ public class ConsonantUtilTest {
     @ValueSource(chars = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'})
     public void testIsAConsonant_withVowels(char vowel) {
         assertThat(ConsonantUtil.isAConsonant(vowel)).isFalse();
+    }
+
+    @Test
+    public void testIsAConsonant_withNonLetter() {
+        assertThat(ConsonantUtil.isAConsonant('1')).isFalse();
+    }
+
+    @Test
+    public void testIsAConsonant_withWhitespace() {
+        assertThat(ConsonantUtil.isAConsonant(' ')).isFalse();
+        assertThat(ConsonantUtil.isAConsonant('\n')).isFalse();
+        assertThat(ConsonantUtil.isAConsonant('\t')).isFalse();
     }
 }
