2c8e2cd0fc715e56820990fb0c69791aa51dccb0
tims <tim.schmitte@realdolmen.com>
2023-04-05 10:18:22 +0200

    finalizing ex7

--- src/main/java/com/realdolmen/refactoring/ex7/Truck.java
+++ src/main/java/com/realdolmen/refactoring/ex7/Truck.java
@@ -2,44 +2,21 @@ package com.realdolmen.refactoring.ex7;
 
 import java.util.List;
 
-public class Truck {
-    private final String make;
-    private final String model;
-    private final int year;
+public class Truck extends Vehicle {
     private final double towingCapacity;
 
     public Truck(String make, String model, int year, double towingCapacity) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
+        super(make, model, year);
         this.towingCapacity = towingCapacity;
-    }
-
-    public void accelerate() {
-        System.out.println("accelerating");
-    }
-
-    public void brake() {
-        System.out.println("braking");
     }
 
     public double getTowingCapacity() {
         return towingCapacity;
     }
 
+    @Override
     public List<String> getSupportedFuelTypes(){
         return List.of("Diesel", "Gasoline", "LPG");
     }
 
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
-    }
 }
