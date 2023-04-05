5033547c3cacbf22cb263a43a0014e9c69e3f359
tims <tim.schmitte@realdolmen.com>
2023-04-05 11:46:41 +0200

    ex7: finished solution

--- src/main/java/com/realdolmen/refactoring/ex7/Car.java
+++ src/main/java/com/realdolmen/refactoring/ex7/Car.java
@@ -2,45 +2,21 @@ package com.realdolmen.refactoring.ex7;
 
 import java.util.List;
 
-public class Car {
-    private final String make;
-    private final String model;
-    private final int year;
+public class Car extends Vehicle {
     private final double trunkSize;
 
     public Car(String make, String model, int year, double trunkSize) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
+        super(make, model, year);
         this.trunkSize = trunkSize;
-    }
-
-    public void accelerate() {
-        System.out.println("accelerating");
-    }
-
-    public void brake() {
-        System.out.println("braking");
     }
 
     public double getTrunkSize() {
         return trunkSize;
     }
 
-    public List<String> getSupportedFuelTypes(){
+    public List<String> getSupportedFuelTypes() {
         return List.of("Diesel", "Gasoline");
     }
 
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
-    }
 }
 
